1026a15b31062866f4b00f4ce7cf8508f8aafb2b
ileis <gabrielileis07@gmail.com>
2026-05-31 04:58:43 -0300

    refact: enum de estados para bottom bar, retira strings hardcoded para string resourses

--- app/src/main/java/com/example/fintrack/ui/components/BottomBar.kt
+++ app/src/main/java/com/example/fintrack/ui/components/BottomBar.kt
@@ -9,33 +9,35 @@ import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.fintrack.R
 
 @Composable
 fun BottomBar(
     currentScreen : String,
     onHomeClick   : () -> Unit,
     onChartsClick : () -> Unit,
-    onBudgetClick : () -> Unit
+    onBudgetClick : () -> Unit,
 ) {
     NavigationBar {
         NavigationBarItem(
-            selected = currentScreen == "home",
+            selected = currentScreen == SelectedScreenState.HOME.value,
             onClick  = onHomeClick,
-            icon     = { Icon(Icons.Outlined.Home, contentDescription = "Home") },
-            label    = { Text("Home") }
+            icon     = { Icon(Icons.Outlined.Home, contentDescription = stringResource(R.string.home)) },
+            label    = { Text(stringResource(R.string.home)) }
         )
         NavigationBarItem(
-            selected = currentScreen == "charts",
+            selected = currentScreen == SelectedScreenState.CHARTS.value,
             onClick  = onChartsClick,
-            icon     = { Icon(Icons.Outlined.BarChart, contentDescription = "Charts") },
-            label    = { Text("Charts") }
+            icon     = { Icon(Icons.Outlined.BarChart, contentDescription = stringResource(R.string.charts)) },
+            label    = { Text(stringResource(R.string.charts)) }
         )
         NavigationBarItem(
-            selected = currentScreen == "budget",
+            selected = currentScreen == SelectedScreenState.BUDGET.value,
             onClick  = onBudgetClick,
-            icon     = { Icon(Icons.Outlined.Savings, contentDescription = "Budget") },
-            label    = { Text("Budget") }
+            icon     = { Icon(Icons.Outlined.Savings, contentDescription = stringResource(R.string.budget)) },
+            label    = { Text(stringResource(R.string.budget)) }
         )
     }
 }
@@ -44,9 +46,15 @@ fun BottomBar(
 @Composable
 fun BottomBarPreview() {
     BottomBar(
-        currentScreen = "home",
+        currentScreen = SelectedScreenState.HOME.value,
         onHomeClick   = {},
         onChartsClick = {},
         onBudgetClick = {}
     )
+}
+
+enum class SelectedScreenState(val value: String) {
+    HOME("home_screen"),
+    CHARTS("charts_screen"),
+    BUDGET("budget_screen")
 }
